8db590b5c36c714fb2f676eac3befa212b8b490d
ExequielTrangoni <exetrangoni89@gmail.com>
2025-11-23 23:43:28 -0300

    Creacion endpoint GET, POST Y DELETE de mascotas. Agregamos conexiones a endpoint en index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,19 @@
-import express from "express";
-import cors from "cors";
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
 
 const app = express();
+const puerto = 3000;
+
+app.use(cors());
 app.use(express.json());
+
+const rutasMascotas = require('./routes/mascotas');
+const rutasAdopciones = require('./routes/adopciones');
+
+app.use('/api/mascotas', rutasMascotas);
+app.use('/api/adopciones', rutasAdopciones);
 
 app.get("/", (req, res) => {
   res.send("Backend anda");
